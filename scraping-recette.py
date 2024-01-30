df33b1ebc4e67e076111d6c90bc893d73610ccef
BalloIbrahima <ibrahimaballo01@gmail.com>
2024-01-30 12:52:11 +0000

    add beautifull soup

--- scraping-recette.py
+++ scraping-recette.py
@@ -1,14 +1,33 @@
+from bs4 import BeautifulSoup
 import requests;
 url="https://codeavecjonathan.com/scraping/recette"
 
 response=requests.get(url)
+# change encoding to apparent_enscoding
+response.encoding=response.apparent_encoding
 
 if response.status_code==200 :
     content=response.text
     print("Resultat -------------------")
     print(content)
+    # write file on file
+    f=open("recette.html",'w')
+    f.write(content)
+    f.close()
 
-print("FIN!!!!!!!!!!!")
+    # Convert content to HTML format for read
+    soup =BeautifulSoup(content,'html5lib')
+    print("After coverted------------------------------------------")
+    print("++++++++++++++++++++++++++++++++ Result ------------------------------------------")
+
+    print(soup)
+    print("++++++++++++++++++++++++++++++++ End Result ------------------------------------------")
+
+
+else:
+    print("error on get page content "+response.status_code)
+
+print("FIN !!!!!!!!!!!")
 
 
 # https://codeavecjonathan.com/scraping/recette
