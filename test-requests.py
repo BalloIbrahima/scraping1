a98a053b3dec70ef390d6f0baea28c51a7556860
BalloIbrahima <ibrahimaballo01@gmail.com>
2024-02-01 11:37:38 +0000

    test amazon

---
+++ test-requests.py
@@ -0,0 +1,29 @@
+import requests;
+
+
+# url="https://codeavecjonathan.com/scraping/techsport"
+url="https://amazon.fr"
+
+def get_element_if_none(e):
+    if e:
+        return e.text.strip()
+    else: return None
+
+HEADERS={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+response=requests.get(url,headers=HEADERS)
+
+
+if response.status_code==200 :
+    content=response.text
+    print("Resultat -------------------")
+    print(content)
+    # write file on file
+    f=open("tes-request.html",'w')
+    f.write(content)
+    f.close()
+
+else:
+    print("error on get page content "+str(response.status_code))
+
+print("FIN !!!!!!!!!!!")
+
