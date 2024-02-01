d99289e8964aecad5aac47096f99ccaa9175f672
BalloIbrahima <ibrahimaballo01@gmail.com>
2024-02-01 12:08:50 +0000

    web-unlocker test ok

--- web-unlocker.py
+++ web-unlocker.py
@@ -1,13 +1,15 @@
 import sys
 import ssl
+
+from personalurl import Personnal
 ssl._create_default_https_context = ssl._create_unverified_context
 url="https://codeavecjonathan.com/scraping/techsport"
 
 import urllib.request
 opener = urllib.request.build_opener(
     urllib.request.ProxyHandler(
-        {'http': 'personnal-url-brdata',
-        'https': 'personnal-url-brdata'}))
+        {'http': Personnal.url,
+        'https': Personnal.url}))
 
 html=opener.open(url).read().decode('utf-8')
 f=open('web-unlocker.html','w')
